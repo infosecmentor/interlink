1d860178ecc10b53ce1de4412ba8bf070ddfe09f
Lewis Mwendwa <lewis.mwendwa@student.moringaschool.com>
2022-09-13 17:31:50 +0300

    Done with the nav-bar tweaks.

--- js/custom.js
+++ js/custom.js
@@ -16,4 +16,23 @@ function myMap() {
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 
+/** button dropdown */
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
